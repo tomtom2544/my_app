93807bd7c83c41bf89dd20a091d3fd085997cfde
Jira Iemlaor <tomtom2544@gmail.com>
2025-02-14 15:07:03 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,18 @@ import 'package:adidas/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(HomeScreen());
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Adidas App',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: HomeScreen(),
+    );
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,11 @@
-import 'package:adidas/home_screen.dart';
+import 'package:adidas/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomeScreen());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
